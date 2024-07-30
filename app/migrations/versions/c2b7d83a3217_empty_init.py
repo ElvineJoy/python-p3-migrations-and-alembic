814638b2e8a37eeffb4445e20d58b9aef38a5ace
ElvineJoy <elvinkuiyungest@gmail.com>
2024-07-30 21:07:36 +0300

    code along understood

---
+++ app/migrations/versions/c2b7d83a3217_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: c2b7d83a3217
+Revises: 
+Create Date: 2024-07-30 20:54:06.170814
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c2b7d83a3217'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
